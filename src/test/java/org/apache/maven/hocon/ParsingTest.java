4c118f89d083b519a130dae180f96fff73acb939
Guillaume Nodet <gnodet@gmail.com>
2024-12-06 14:59:50 +0100

    Support short gav syntax for dependencies and parent, parse xml config

--- src/test/java/org/apache/maven/hocon/ParsingTest.java
+++ src/test/java/org/apache/maven/hocon/ParsingTest.java
@@ -22,10 +22,14 @@ import java.io.File;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import org.apache.maven.api.model.Dependency;
 import org.apache.maven.api.model.Model;
+import org.apache.maven.api.model.Plugin;
+import org.apache.maven.api.xml.XmlNode;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ParsingTest {
 
@@ -38,5 +42,27 @@ class ParsingTest {
         assertEquals("40", model.getParent().getVersion());
         assertEquals(1, model.getDependencies().size());
         assertEquals(2, model.getProperties().size());
+    }
+
+    @Test
+    void testParse2() throws Exception {
+        Config config = ConfigFactory.parseFile(new File("src/test/resources/pom2.hocon"));
+
+        Model model = new HoconReader().parseModel(config.root());
+
+        assertEquals("40", model.getParent().getVersion());
+        assertEquals(2, model.getDependencies().size());
+        Dependency dependency = model.getDependencies().get(0);
+        assertEquals("org.apache.maven", dependency.getGroupId());
+        assertEquals("maven-api-core", dependency.getArtifactId());
+        assertEquals("${maven.version}", dependency.getVersion());
+        assertEquals(1, model.getBuild().getPlugins().size());
+        Plugin plugin = model.getBuild().getPlugins().get(0);
+        assertEquals("org.apache.maven", plugin.getGroupId());
+        assertEquals("maven-compiler-plugin", plugin.getArtifactId());
+        assertEquals("${maven.compiler.version}", plugin.getVersion());
+        XmlNode node = plugin.getConfiguration();
+        assertNotNull(node);
+        assertEquals("17", node.getChild("release").getValue());
     }
 }
